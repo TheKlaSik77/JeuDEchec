bd1cb471bbda5d6912e5a6340d0551a2dde00d08
ybenyoussef <yasmine.benyoussef01@gmail.com>
2022-12-08 16:04:54 +0100

    deplacementPion methode

--- src/DeplacementPion.java
+++ src/DeplacementPion.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 
 public class DeplacementPion {
 
-
     /**
      * @param echiquier
      * @return vrai si le pion peut avancer de 2 cases
      */
     public static boolean avance2Cases(int[][] echiquier, int ligne, int colonne) {
-        if (echiquier[6][colonne] == 1)
+
+        if (echiquier[6][colonne] == 1) //piece blanc
             return true;
-        else
+        else if (echiquier[1][colonne] == -1) // pièce noire
+            return true;
+        else {
             return false;
+        }
     }
-
-
 }
 
