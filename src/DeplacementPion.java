d154f4ee64161f370900668ec47d424a54f920c9
TheKlaSik77 <kylian77280@gmail.com>
2022-12-11 13:15:58 +0100

    Correction du nom de la fonction peutAvancer2Cases

--- src/DeplacementPion.java
+++ src/DeplacementPion.java
@@ -7,7 +7,7 @@ public class DeplacementPion {
      * @param echiquier
      * @return vrai si le pion peut avancer de 2 cases
      */
-    public static boolean avance2Cases(int[][] echiquier, int colonne) {
+    public static boolean Peutavancer2Cases(int[][] echiquier, int colonne) {
 
         if (echiquier[6][colonne] == 1 || echiquier[1][colonne] == -1) //piece blanche
             return true;
