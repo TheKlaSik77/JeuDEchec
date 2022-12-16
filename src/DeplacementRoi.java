abe7ee9e0079e7a85201497b39c433bcff12c49c
Kylian <kylian77280@gmail.com>
2022-12-16 11:42:20 +0100

    Corrections

--- src/DeplacementRoi.java
+++ src/DeplacementRoi.java
@@ -2,6 +2,10 @@ public class DeplacementRoi {
 
     public static boolean peutRoque = true;
 
+    public static void nePeutPlusRoque() {
+        peutRoque = false;
+    }
+
     /**
      * Teste pour un déplacement du roi s'il peut y aller et s'il est mis en échec
      * @param echiquier
@@ -21,16 +25,53 @@ public class DeplacementRoi {
         echiquierTemp[ligneDepart][colonneDepart] = 0;
         echiquierTemp[ligneArrivee][colonneArrivee] = echiquier[ligneDepart][colonneDepart];
 
-        for (int ligne = 0 ; ligne < echiquierTemp.length ; ligne++){
-            for (int colonne = 0 ; colonne < echiquierTemp[ligne].length ; colonne++){
-                if (echiquierTemp[ligne][colonne] < 0){
-                    if(MethodesDeplacements.testPieceMetEnEchec(echiquierTemp,ligne,colonne)){
-                        return false;
+        if(echiquier[ligneDepart][colonneDepart] == 2) {
+            for (int ligne = 0; ligne < echiquierTemp.length; ligne++) {
+                for (int colonne = 0; colonne < echiquierTemp[ligne].length; colonne++) {
+                    if (echiquierTemp[ligne][colonne] < 0) {
+                        if (MethodesDeplacements.testPieceMetEnEchec(echiquierTemp, ligne, colonne)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        } else {
+            for (int ligne = 0; ligne < echiquierTemp.length; ligne++) {
+                for (int colonne = 0; colonne < echiquierTemp[ligne].length; colonne++) {
+                    if (echiquierTemp[ligne][colonne] > 0) {
+                        if (MethodesDeplacements.testPieceMetEnEchec(echiquierTemp, ligne, colonne)) {
+                            return false;
+                        }
                     }
                 }
             }
         }
+        return true;
+    }
 
+    public static void deplacerRoi(int[][] echiquier, int ligneDepart, int colonneDepart, int ligneArrivee, int colonneArrivee){
+
+        if (echiquier[ligneDepart][colonneDepart] == 2){
+            echiquier[ligneDepart][colonneDepart] = 0;
+            echiquier[ligneArrivee][colonneArrivee] = 2;
+        } else if (echiquier[ligneDepart][colonneDepart] == -2){
+            echiquier[ligneDepart][colonneDepart] = 0;
+            echiquier[ligneArrivee][colonneArrivee] = -2;
+        }
+    }
+
+    public static boolean roiMetEnEchecRoi(int[][] echiquier, int ligne, int colonne){
+
+        int roi;
+        if (echiquier[ligne][colonne] == 2) { // Roi blanche
+            roi = -2;
+        } else {
+            roi = 2;
+        }
+
+        if (echiquier[ligne+1][colonne] == roi || echiquier[ligne-1][colonne] == roi || echiquier[ligne][colonne+1] == roi || echiquier[ligne][colonne-1] == roi || echiquier[ligne+1][colonne+1] == roi || echiquier[ligne+1][colonne-1] == roi || echiquier[ligne-1][colonne+1] == roi || echiquier[ligne-1][colonne-1] == roi){
+            return true;
+        }
     }
 
 
